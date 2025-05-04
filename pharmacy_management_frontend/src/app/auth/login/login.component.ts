23434644c52b67d54465ec05c555b6bb1adb3025
Omar Faruq <omarfaruq9723@gmail.com>
2025-05-05 02:22:04 +0600

    init

--- pharmacy_management_frontend/src/app/auth/login/login.component.ts
+++ pharmacy_management_frontend/src/app/auth/login/login.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
+
 export class LoginComponent {
+  email = '';
+  password = '';
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  onLogin() {
+    this.auth.login({ email: this.email, password: this.password })
+      .subscribe({
+        next: (res) => {
+          this.auth.setToken(res.access_token);
+          this.router.navigate(['/home']);
+        },
+        error: (error) => {
+          alert('Invalid credentials')
+          console.log(error)
+        }
+      });
+  }
 
 }
