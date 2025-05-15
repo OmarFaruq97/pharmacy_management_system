92f6caa5d365da20fd86ec3d869bc3c96ebcbe7e
Omar Faruq <omarfaruq9723@gmail.com>
2025-05-15 06:02:33 +0600

    INVOICE IS WORKING BETTER ALHAMDULILLAH

---
+++ pharmacy_management_frontend/src/app/core/invoice.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InvoiceService {
+  private baseUrl = 'http://localhost:8080/api/invoice';
+
+  constructor(private http: HttpClient) {}
+
+  createInvoices(data: any[]): Observable<any> {
+    return this.http.post(`${this.baseUrl}/create`, data);
+  }
+
+  getAllInvoices(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/all`);
+  }
+
+  updateInvoice(invoiceNumber: string, data: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/update/${invoiceNumber}`, data);
+  }
+
+  deleteInvoice(invoiceNumber: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/delete/${invoiceNumber}`);
+  }
+}
