de3378b81f31ee2e43beeb7badd67c866cbba973
Omar Faruq <omarfaruq9723@gmail.com>
2025-06-23 22:26:36 +0600

    access new

--- pharmacy_management_frontend/src/app/access-new/access-new.component.ts
+++ pharmacy_management_frontend/src/app/access-new/access-new.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { ReceivedMedicieneComponent } from "../received-mediciene/received-mediciene.component";
+import { CompanyNameComponent } from "../company-name/company-name.component";
+import { MedicineNameComponent } from "../medicine-name/medicine-name.component";
+import { NewGenericComponent } from "../new-generic/new-generic.component";
+import { NewCategoryComponent } from "../new-category/new-category.component";
 
 @Component({
   selector: 'app-access-new',
-  imports: [],
+  imports: [CompanyNameComponent, MedicineNameComponent, NewGenericComponent, NewCategoryComponent],
   templateUrl: './access-new.component.html',
   styleUrl: './access-new.component.css'
 })
